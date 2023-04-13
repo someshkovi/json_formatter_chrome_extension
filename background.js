cb4c8ad2a6aacc7cb987315ed9bab965d0053613
someshkovi <someshkovi@gmail.com>
2023-04-13 21:54:09 +0530

    icon fixes, general improvements

--- background.js
+++ background.js
@@ -1,3 +1,21 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "openJson",
+    title: "Show Json",
+    contexts: ["selection"],
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  const jsonString = info.selectionText;
+  chrome.tabs.create({
+    url:
+      chrome.runtime.getURL("index.html") +
+      "?json=" +
+      encodeURIComponent(jsonString),
+  });
+});
+
 chrome.action.onClicked.addListener(function () {
-    chrome.tabs.create({ url: 'index.html' });
+  chrome.tabs.create({ url: "index.html" });
 });
